a087665c82a6fa3b3aec145c30dd45c9930bec9b
Addy <aba@efiware.com>
2020-11-02 13:27:14 +0100

    barchart implemented

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
@@ -11,8 +12,7 @@ var myChart = new Chart(ctx, {
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(153, 102, 255, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -20,7 +20,6 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
@@ -34,4 +33,101 @@ var myChart = new Chart(ctx, {
             }]
         }
     }
+});
+
+*/
+
+gradientBarChartConfiguration = {
+  maintainAspectRatio: false,
+  legend: {
+    display: false
+  },
+
+  tooltips: {
+    backgroundColor: '#f5f5f5',
+    titleFontColor: '#333',
+    bodyFontColor: '#666',
+    bodySpacing: 4,
+    xPadding: 12,
+    mode: "nearest",
+    intersect: 0,
+    position: "nearest"
+  },
+  responsive: true,
+  scales: {
+    yAxes: [{
+
+      gridLines: {
+        drawBorder: false,
+        color: 'rgba(29,140,248,0.1)',
+        zeroLineColor: "transparent",
+      },
+      ticks: {
+        suggestedMin: 60,
+        suggestedMax: 120,
+        padding: 20,
+        fontColor: "#9e9e9e"
+      }
+    }],
+
+    xAxes: [{
+
+      gridLines: {
+        drawBorder: false,
+        color: 'rgba(29,140,248,0.1)',
+        zeroLineColor: "transparent",
+      },
+      ticks: {
+        padding: 20,
+        fontColor: "#9e9e9e"
+      }
+    }]
+  }
+};
+
+
+var ctx = document.getElementById("salesByCountryChart").getContext("2d");
+
+var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
+gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
+gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
+
+
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  responsive: true,
+  legend: {
+    display: false
+  },
+  data: {
+    labels: ['DENMARK', 'UK', 'GERMANY', 'NORWAY', 'FRANCE'],
+    datasets: [{
+      label: "Cars sold",
+      fill: true,
+     
+      hoverBackgroundColor: gradientStroke,
+      
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)'
+    ],
+    borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+    ],
+      borderWidth: 2,
+      borderDash: [],
+      borderDashOffset: 0.0,
+      data: [150,75,89,35,45],
+    }]
+  },
+  options: gradientBarChartConfiguration
 });
