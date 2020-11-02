247bd9aad4651b0447e39ab159f9587da6e5a2c4
Addy <aba@efiware.com>
2020-11-02 10:57:07 +0100

    country flags & table implemented

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-//Bar Chart
-var ctx = document.getElementById("bcaChart").getContext('2d');
+var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["M", "T", "W", "T", "F", "S", "S"],
-    datasets: [{
-      label: 'apples',
-      data: [12, 19, 3, 17, 28, 24, 7],
-      backgroundColor: "rgba(153,255,51,1)"
-    }, {
-      label: 'oranges',
-      data: [30, 29, 5, 5, 20, 3, 10],
-      backgroundColor: "rgba(255,153,0,1)"
-    }]
-  }
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
